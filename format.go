2eca5bfea4972adfafd9ae7c58c94a98fccaa15b
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-08-31 12:59:52 -0600

    Implement and unit test a HTML formatter (htmlFormatterImpl).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- format.go
+++ format.go
@@ -3,11 +3,15 @@ package playground
 import (
   "bytes"
   "encoding/json"
+  "errors"
+  "fmt"
+  xhtml "golang.org/x/net/html"
   "html"
   "io"
   "log/slog"
   "regexp"
   "strings"
+  "unicode/utf8"
 )
 
 type textFormatterImpl struct{}
@@ -158,5 +162,169 @@ func xmlTagReplacer(indent string) func([]byte) []byte {
       wasEndOfTag = true
       return tag
     }
+  }
+}
+
+type htmlFormatterImpl struct{}
+
+var (
+  // htmlReStartingBlankBeforeText matches a string that starts with leading blank followed by a non-blank character
+  htmlReStartingBlankBeforeText = regexp.MustCompile(`^\s+\S`)
+
+  // htmlReTrailingBlankAfterText matches a string that ends with a non-blank character followed by trailing blank
+  htmlReTrailingBlankAfterText = regexp.MustCompile(`\S\s+$`)
+
+  // inlineTags is a dictionary that contains tags that typically do not start on a new line and only occupy the space
+  // required by their content.
+  inlineTags = map[string]struct{}{
+    "a": {}, "b": {}, "i": {}, "em": {}, "strong": {}, "code": {}, "span": {},
+    "ins": {}, "big": {}, "small": {}, "tt": {}, "abbr": {}, "acronym": {},
+    "cite": {}, "dfn": {}, "kbd": {}, "samp": {}, "var": {}, "bdo": {},
+    "map": {}, "q": {}, "sub": {}, "sup": {},
+  }
+
+  // selfClosingTags is a dictionary that contains tags that do not have a closing counterpart.
+  selfClosingTags = map[string]struct{}{
+    "input": {}, "link": {}, "meta": {}, "hr": {}, "img": {}, "br": {}, "area": {},
+    "base": {}, "col": {}, "param": {}, "command": {}, "embed": {}, "keygen": {},
+    "source": {}, "track": {}, "wbr": {},
+  }
+)
+
+// isInlineTag checks if the given HTML tag is an inline tag.
+func isInlineTag(tag []byte) bool {
+  _, found := inlineTags[string(tag)]
+  return found
+}
+
+// isSelfClosingTag checks if the given HTML tag is a self-closing tag.
+func isSelfClosingTag(tag []byte) bool {
+  _, found := selfClosingTags[string(tag)]
+  return found
+}
+
+// adjustTextIndentation adjusts the indentation of a block of text. It first determines the minimum indentation level
+// (leading spaces) present in the non-empty lines of the text; then it removes this common indentation and re-indents
+// the text with a specified depth of spaces.
+func adjustTextIndentation(text []byte, depth int) []byte {
+  minIndent := 1000
+
+  for _, line := range bytes.Split(text, []byte{'\n'}) {
+    if trimmed := bytes.TrimLeft(line, " "); len(trimmed) > 0 {
+      indent := len(line) - len(trimmed)
+      if indent < minIndent {
+        minIndent = indent
+      }
+    }
+  }
+
+  if minIndent == 1000 {
+    minIndent = 0
+  }
+
+  re := regexp.MustCompile(fmt.Sprintf(`\n\s{%d}`, minIndent))
+  return re.ReplaceAllLiteral(text, append([]byte{'\n'}, bytes.Repeat([]byte("  "), depth)...))
+}
+
+// format formats the HTML input with proper indentation.
+func (htmlFormatterImpl) format(input []byte, output io.Writer, indent string) {
+  var (
+    reader           = bytes.NewReader(input)
+    tokenizer        = xhtml.NewTokenizer(reader)
+    depth            = 0
+    prevType         = xhtml.ErrorToken
+    tagName          []byte
+    prvName          []byte
+    longText         = false
+    skipFirstNewline = true
+  )
+
+  writeIndent := func(depth int) {
+    if skipFirstNewline {
+      skipFirstNewline = false
+    } else {
+      output.Write([]byte("\n"))
+    }
+
+    output.Write(bytes.Repeat([]byte(indent), depth))
+  }
+
+  for {
+    tokenType := tokenizer.Next()
+
+    if tokenType != xhtml.TextToken {
+      prvName = tagName
+      tagName, _ = tokenizer.TagName()
+    }
+
+    switch tokenType {
+    case xhtml.ErrorToken:
+      if errors.Is(tokenizer.Err(), io.EOF) {
+        return
+      }
+
+      slog.Error(tokenizer.Err().Error())
+      return
+
+    case xhtml.StartTagToken:
+      if !(isInlineTag(tagName) && prevType == xhtml.TextToken) {
+        writeIndent(depth)
+      }
+
+      output.Write(tokenizer.Raw())
+      if !isSelfClosingTag(tagName) {
+        depth++
+      }
+
+    case xhtml.SelfClosingTagToken, xhtml.CommentToken, xhtml.DoctypeToken:
+      writeIndent(depth)
+      output.Write(tokenizer.Raw())
+
+    case xhtml.EndTagToken:
+      if depth > 0 {
+        depth--
+      }
+
+      if !bytes.Equal(prvName, tagName) || prevType == xhtml.SelfClosingTagToken || prevType == xhtml.CommentToken ||
+        prevType == xhtml.DoctypeToken || (prevType == xhtml.TextToken && longText) {
+        writeIndent(depth)
+      }
+
+      output.Write(tokenizer.Raw())
+
+    case xhtml.TextToken:
+      t := bytes.Replace(tokenizer.Raw(), []byte{'\t'}, []byte(indent), -1)
+      text := bytes.Trim(t, "\n\r ")
+
+      if htmlReTrailingBlankAfterText.Match(t) {
+        text = append(text, ' ')
+      }
+
+      longText = false
+      if len(text) > 0 {
+        if bytes.Contains(text, []byte{'\n'}) {
+          if !(prevType == xhtml.EndTagToken && isInlineTag(tagName)) {
+            writeIndent(depth)
+          } else if htmlReStartingBlankBeforeText.Match(t) {
+            text = append([]byte{' '}, text...)
+          }
+
+          output.Write(adjustTextIndentation(text, depth))
+          longText = true
+        } else {
+          if utf8.RuneCount(text) > 80 || prevType != xhtml.StartTagToken {
+            if !(prevType == xhtml.EndTagToken && isInlineTag(tagName)) {
+              writeIndent(depth)
+              longText = true
+            } else if htmlReStartingBlankBeforeText.Match(t) {
+              text = append([]byte{' '}, text...)
+            }
+          }
+          output.Write(text)
+        }
+      }
+    }
+
+    prevType = tokenType
   }
 }
